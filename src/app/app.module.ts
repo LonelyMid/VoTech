ba0dcb5cf57cbb3a8ca6d3ce1fbcf15807b30f79
LonelyMid <gabrielevaine@gmail.com>
2019-05-17 19:53:07 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,22 @@ import { RegistrationComponent } from './user/registration/registration.componen
 import { UserService } from './shared/user.service';
 import { LoginComponent } from './user/login/login.component';
 import { HomeComponent } from './home/home.component';
-
+import { MainComponent } from './main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     RegistrationComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule, 
     ToastrModule.forRoot({
       progressBar: true
     })
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,3 +21,4 @@ export class AuthGuard implements CanActivate {
 
   }
 }
+
